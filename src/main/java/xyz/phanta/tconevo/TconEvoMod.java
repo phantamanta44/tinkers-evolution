176ac5ae311c763b58cc2d8b0b741d4b7a0836ef
phantamanta44 <evandalong@gmail.com>
2020-05-27 03:17:20 -0500

    Bump up to 1.0.2 (should have done this a few commits ago)

--- src/main/java/xyz/phanta/tconevo/TconEvoMod.java
+++ src/main/java/xyz/phanta/tconevo/TconEvoMod.java
@@ -16,7 +16,7 @@ import xyz.phanta.tconevo.item.ItemMaterial;
 public class TconEvoMod extends Virtue {
 
     public static final String MOD_ID = "tconevo";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     @SuppressWarnings("NotNullFieldNotInitialized")
     @Mod.Instance(MOD_ID)
