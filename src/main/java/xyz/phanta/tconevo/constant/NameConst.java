2b0cc3ceacd6655bbfccd1f03bb3e938c322fa76
phantamanta44 <evandalong@gmail.com>
2020-05-28 18:40:54 -0500

    Prefix all trait ids with the mod id to prevent conflicts (fixes #1)

--- src/main/java/xyz/phanta/tconevo/constant/NameConst.java
+++ src/main/java/xyz/phanta/tconevo/constant/NameConst.java
@@ -1,5 +1,7 @@
 package xyz.phanta.tconevo.constant;
 
+import xyz.phanta.tconevo.TconEvoMod;
+
 public class NameConst {
 
     public static final String ITEM_MATERIAL = "material";
@@ -13,30 +15,30 @@ public class NameConst {
     public static final String POTION_IMMORTALITY = "immortality";
     public static final String POTION_MORTAL_WOUNDS = "mortal_wounds";
 
-    public static final String TRAIT_AFTERSHOCK = "aftershock";
-    public static final String TRAIT_CASCADING = "cascading";
-    public static final String TRAIT_DEADLY_PRECISION = "deadly_precision";
-    public static final String TRAIT_IMPACT_FORCE = "impact_force";
-    public static final String TRAIT_LUMINIFEROUS = "luminiferous";
-    public static final String TRAIT_MORTAL_WOUNDS = "mortal_wounds";
-    public static final String TRAIT_OPPORTUNIST = "opportunist";
-    public static final String TRAIT_REJUVENATING = "rejuvenating";
-    public static final String TRAIT_RELENTLESS = "relentless";
-    public static final String TRAIT_STAGGERING = "staggering";
-    public static final String TRAIT_SUNDERING = "sundering";
+    public static final String TRAIT_AFTERSHOCK = TconEvoMod.MOD_ID + ".aftershock";
+    public static final String TRAIT_CASCADING = TconEvoMod.MOD_ID + ".cascading";
+    public static final String TRAIT_DEADLY_PRECISION = TconEvoMod.MOD_ID + ".deadly_precision";
+    public static final String TRAIT_IMPACT_FORCE = TconEvoMod.MOD_ID + ".impact_force";
+    public static final String TRAIT_LUMINIFEROUS = TconEvoMod.MOD_ID + ".luminiferous";
+    public static final String TRAIT_MORTAL_WOUNDS = TconEvoMod.MOD_ID + ".mortal_wounds";
+    public static final String TRAIT_OPPORTUNIST = TconEvoMod.MOD_ID + ".opportunist";
+    public static final String TRAIT_REJUVENATING = TconEvoMod.MOD_ID + ".rejuvenating";
+    public static final String TRAIT_RELENTLESS = TconEvoMod.MOD_ID + ".relentless";
+    public static final String TRAIT_STAGGERING = TconEvoMod.MOD_ID + ".staggering";
+    public static final String TRAIT_SUNDERING = TconEvoMod.MOD_ID + ".sundering";
 
     private static final String ARMOUR_SUFFIX = "_armor";
-    public static final String TRAIT_BULWARK = "bulwark", ARMOUR_TRAIT_BULWARK = TRAIT_BULWARK + ARMOUR_SUFFIX;
-    public static final String TRAIT_CELESTIAL = "celestial", ARMOUR_TRAIT_CELESTSIAL = TRAIT_CELESTIAL + ARMOUR_SUFFIX;
-    public static final String TRAIT_CHILLING_TOUCH = "chilling_touch", ARMOUR_TRAIT_CHILLING_TOUCH = TRAIT_CHILLING_TOUCH + ARMOUR_SUFFIX;
-    public static final String TRAIT_DIVINE_GRACE = "divine_grace", ARMOUR_TRAIT_DIVINE_GRACE = TRAIT_DIVINE_GRACE + ARMOUR_SUFFIX;
-    public static final String TRAIT_GALE_FORCE = "gale_force", ARMOUR_TRAIT_GALE_FORCE = TRAIT_GALE_FORCE + ARMOUR_SUFFIX;
-    public static final String TRAIT_HEARTH_EMBRACE = "hearth_embrace", ARMOUR_TRAIT_HEARTH_EMBRACE = TRAIT_HEARTH_EMBRACE + ARMOUR_SUFFIX;
-    public static final String TRAIT_RADIANT = "radiant", ARMOUR_TRAIT_RADIANT = TRAIT_RADIANT + ARMOUR_SUFFIX;
-    public static final String TRAIT_SECOND_WIND = "second_wind", ARMOUR_TRAIT_SECOND_WIND = TRAIT_SECOND_WIND + ARMOUR_SUFFIX;
-    public static final String TRAIT_STIFLING = "stifling", ARMOUR_TRAIT_STIFLING = TRAIT_STIFLING + ARMOUR_SUFFIX;
-    public static final String TRAIT_STONEBOUND = "stonebound", ARMOUR_TRAIT_STONEBOUND = TRAIT_STONEBOUND + ARMOUR_SUFFIX;
-    public static final String TRAIT_WILL_STRENGTH = "will_strength", ARMOUR_TRAIT_WILL_STRENGTH = TRAIT_WILL_STRENGTH + ARMOUR_SUFFIX;
+    public static final String TRAIT_BULWARK = TconEvoMod.MOD_ID + ".bulwark", ARMOUR_TRAIT_BULWARK = TRAIT_BULWARK + ARMOUR_SUFFIX;
+    public static final String TRAIT_CELESTIAL = TconEvoMod.MOD_ID + ".celestial", ARMOUR_TRAIT_CELESTSIAL = TRAIT_CELESTIAL + ARMOUR_SUFFIX;
+    public static final String TRAIT_CHILLING_TOUCH = TconEvoMod.MOD_ID + ".chilling_touch", ARMOUR_TRAIT_CHILLING_TOUCH = TRAIT_CHILLING_TOUCH + ARMOUR_SUFFIX;
+    public static final String TRAIT_DIVINE_GRACE = TconEvoMod.MOD_ID + ".divine_grace", ARMOUR_TRAIT_DIVINE_GRACE = TRAIT_DIVINE_GRACE + ARMOUR_SUFFIX;
+    public static final String TRAIT_GALE_FORCE = TconEvoMod.MOD_ID + ".gale_force", ARMOUR_TRAIT_GALE_FORCE = TRAIT_GALE_FORCE + ARMOUR_SUFFIX;
+    public static final String TRAIT_HEARTH_EMBRACE = TconEvoMod.MOD_ID + ".hearth_embrace", ARMOUR_TRAIT_HEARTH_EMBRACE = TRAIT_HEARTH_EMBRACE + ARMOUR_SUFFIX;
+    public static final String TRAIT_RADIANT = TconEvoMod.MOD_ID + ".radiant", ARMOUR_TRAIT_RADIANT = TRAIT_RADIANT + ARMOUR_SUFFIX;
+    public static final String TRAIT_SECOND_WIND = TconEvoMod.MOD_ID + ".second_wind", ARMOUR_TRAIT_SECOND_WIND = TRAIT_SECOND_WIND + ARMOUR_SUFFIX;
+    public static final String TRAIT_STIFLING = TconEvoMod.MOD_ID + ".stifling", ARMOUR_TRAIT_STIFLING = TRAIT_STIFLING + ARMOUR_SUFFIX;
+    public static final String TRAIT_STONEBOUND = TconEvoMod.MOD_ID + ".stonebound", ARMOUR_TRAIT_STONEBOUND = TRAIT_STONEBOUND + ARMOUR_SUFFIX;
+    public static final String TRAIT_WILL_STRENGTH = TconEvoMod.MOD_ID + ".will_strength", ARMOUR_TRAIT_WILL_STRENGTH = TRAIT_WILL_STRENGTH + ARMOUR_SUFFIX;
 
     // botania
 
@@ -48,18 +50,21 @@ public class NameConst {
     public static final String MAT_ELEMENTIUM = "elementium";
     public static final String MAT_MANA_STRING = "mana_string";
 
-    public static final String TRAIT_MANA_INFUSED = "mana_infused";
-    public static final String TRAIT_AURA_SIPHON = "aura_siphon";
-    public static final String TRAIT_FAE_VOICE = "fae_voice";
-    public static final String TRAIT_GAIA_WRATH = "gaia_wrath";
+    public static final String TRAIT_MANA_INFUSED = TconEvoMod.MOD_ID + ".mana_infused";
+    public static final String TRAIT_AURA_SIPHON = TconEvoMod.MOD_ID + ".aura_siphon";
+    public static final String TRAIT_FAE_VOICE = TconEvoMod.MOD_ID + ".fae_voice";
+    public static final String TRAIT_GAIA_WRATH = TconEvoMod.MOD_ID + ".gaia_wrath";
 
     public static final String ARMOUR_TRAIT_MANA_INFUSED = TRAIT_MANA_INFUSED + ARMOUR_SUFFIX;
     public static final String ARMOUR_TRAIT_FAE_VOICE = TRAIT_FAE_VOICE + ARMOUR_SUFFIX;
-    public static final String TRAIT_AURA_INFUSED = "aura_infused", ARMOUR_TRAIT_AURA_INFUSED = TRAIT_AURA_INFUSED + ARMOUR_SUFFIX;
-    public static final String TRAIT_MANA_AFFINITY = "mana_affinity", ARMOUR_TRAIT_MANA_AFFINITY = TRAIT_MANA_AFFINITY + ARMOUR_SUFFIX;
-    public static final String MOD_GAIA_WILL_AHRIM = "gaia_will_ahrim", MOD_GAIA_WILL_DHAROK = "gaia_will_dharok",
-            MOD_GAIA_WILL_GUTHAN = "gaia_will_guthan", MOD_GAIA_WILL_TORAG = "gaia_will_torag",
-            MOD_GAIA_WILL_VERAC = "gaia_will_verac", MOD_GAIA_WILL_KARIL = "gaia_will_karil";
+    public static final String TRAIT_AURA_INFUSED = TconEvoMod.MOD_ID + ".aura_infused", ARMOUR_TRAIT_AURA_INFUSED = TRAIT_AURA_INFUSED + ARMOUR_SUFFIX;
+    public static final String TRAIT_MANA_AFFINITY = TconEvoMod.MOD_ID + ".mana_affinity", ARMOUR_TRAIT_MANA_AFFINITY = TRAIT_MANA_AFFINITY + ARMOUR_SUFFIX;
+    public static final String MOD_GAIA_WILL_AHRIM = TconEvoMod.MOD_ID + ".gaia_will_ahrim";
+    public static final String MOD_GAIA_WILL_DHAROK = TconEvoMod.MOD_ID + ".gaia_will_dharok";
+    public static final String MOD_GAIA_WILL_GUTHAN = TconEvoMod.MOD_ID + ".gaia_will_guthan";
+    public static final String MOD_GAIA_WILL_TORAG = TconEvoMod.MOD_ID + ".gaia_will_torag";
+    public static final String MOD_GAIA_WILL_VERAC = TconEvoMod.MOD_ID + ".gaia_will_verac";
+    public static final String MOD_GAIA_WILL_KARIL = TconEvoMod.MOD_ID + ".gaia_will_karil";
 
     // draconic evolution
 
@@ -68,34 +73,34 @@ public class NameConst {
     public static final String MAT_DRACONIC_METAL = "draconic_metal";
     public static final String MAT_CHAOTIC_METAL = "chaotic_metal";
 
-    public static final String TRAIT_SOUL_REND = "soul_rend";
-    public static final String TRAIT_EVOLVED = "evolved";
+    public static final String TRAIT_SOUL_REND = TconEvoMod.MOD_ID + ".soul_rend";
+    public static final String TRAIT_EVOLVED = TconEvoMod.MOD_ID + ".evolved";
 
-    public static final String MOD_DRACONIC_ENERGY = "draconic_energy";
-    public static final String MOD_DRACONIC_DIG_SPEED = "draconic_dig_speed";
-    public static final String MOD_DRACONIC_DIG_AOE = "draconic_dig_aoe";
-    public static final String MOD_DRACONIC_ATTACK_DAMAGE = "draconic_attack_damage";
-    public static final String MOD_DRACONIC_ATTACK_AOE = "draconic_attack_aoe";
-    public static final String MOD_DRACONIC_ARROW_DAMAGE = "draconic_arrow_damage";
-    public static final String MOD_DRACONIC_DRAW_SPEED = "draconic_draw_speed";
-    public static final String MOD_DRACONIC_ARROW_SPEED = "draconic_arrow_speed";
-    public static final String MOD_REAPING = "reaping";
-    public static final String MOD_ENTROPIC = "entropic";
-    public static final String MOD_FLUX_BURN = "flux_burn";
-    public static final String MOD_PRIMORDIAL = "primordial";
+    public static final String MOD_DRACONIC_ENERGY = TconEvoMod.MOD_ID + ".draconic_energy";
+    public static final String MOD_DRACONIC_DIG_SPEED = TconEvoMod.MOD_ID + ".draconic_dig_speed";
+    public static final String MOD_DRACONIC_DIG_AOE = TconEvoMod.MOD_ID + ".draconic_dig_aoe";
+    public static final String MOD_DRACONIC_ATTACK_DAMAGE = TconEvoMod.MOD_ID + ".draconic_attack_damage";
+    public static final String MOD_DRACONIC_ATTACK_AOE = TconEvoMod.MOD_ID + ".draconic_attack_aoe";
+    public static final String MOD_DRACONIC_ARROW_DAMAGE = TconEvoMod.MOD_ID + ".draconic_arrow_damage";
+    public static final String MOD_DRACONIC_DRAW_SPEED = TconEvoMod.MOD_ID + ".draconic_draw_speed";
+    public static final String MOD_DRACONIC_ARROW_SPEED = TconEvoMod.MOD_ID + ".draconic_arrow_speed";
+    public static final String MOD_REAPING = TconEvoMod.MOD_ID + ".reaping";
+    public static final String MOD_ENTROPIC = TconEvoMod.MOD_ID + ".entropic";
+    public static final String MOD_FLUX_BURN = TconEvoMod.MOD_ID + ".flux_burn";
+    public static final String MOD_PRIMORDIAL = TconEvoMod.MOD_ID + ".primordial";
 
     public static final String ARMOUR_TRAIT_EVOLVED = TRAIT_EVOLVED + ARMOUR_SUFFIX;
     public static final String ARMOUR_MOD_DRACONIC_ENERGY = MOD_DRACONIC_ENERGY + ARMOUR_SUFFIX;
-    public static final String MOD_DRACONIC_SHIELD_CAPACITY = "draconic_shield_capacity";
+    public static final String MOD_DRACONIC_SHIELD_CAPACITY = TconEvoMod.MOD_ID + ".draconic_shield_capacity";
     public static final String ARMOUR_MOD_DRACONIC_SHIELD_CAPACITY = MOD_DRACONIC_SHIELD_CAPACITY + ARMOUR_SUFFIX;
-    public static final String MOD_DRACONIC_SHIELD_RECOVERY = "draconic_shield_recovery";
+    public static final String MOD_DRACONIC_SHIELD_RECOVERY = TconEvoMod.MOD_ID + ".draconic_shield_recovery";
     public static final String ARMOUR_MOD_DRACONIC_SHIELD_RECOVERY = MOD_DRACONIC_SHIELD_RECOVERY + ARMOUR_SUFFIX;
-    public static final String MOD_DRACONIC_MOVE_SPEED = "draconic_move_speed";
+    public static final String MOD_DRACONIC_MOVE_SPEED = TconEvoMod.MOD_ID + ".draconic_move_speed";
     public static final String ARMOUR_MOD_DRACONIC_MOVE_SPEED = MOD_DRACONIC_MOVE_SPEED + ARMOUR_SUFFIX;
-    public static final String MOD_DRACONIC_JUMP_BOOST = "draconic_jump_boost";
+    public static final String MOD_DRACONIC_JUMP_BOOST = TconEvoMod.MOD_ID + ".draconic_jump_boost";
     public static final String ARMOUR_MOD_DRACONIC_JUMP_BOOST = MOD_DRACONIC_JUMP_BOOST + ARMOUR_SUFFIX;
-    public static final String MOD_CHAOS_RESISTANCE = "chaos_resistance", ARMOUR_MOD_CHAOS_RESISTANCE = MOD_CHAOS_RESISTANCE + ARMOUR_SUFFIX;
-    public static final String MOD_FINAL_GUARD = "final_guard", ARMOUR_MOD_FINAL_GUARD = MOD_FINAL_GUARD + ARMOUR_SUFFIX;
+    public static final String MOD_CHAOS_RESISTANCE = TconEvoMod.MOD_ID + ".chaos_resistance", ARMOUR_MOD_CHAOS_RESISTANCE = MOD_CHAOS_RESISTANCE + ARMOUR_SUFFIX;
+    public static final String MOD_FINAL_GUARD = TconEvoMod.MOD_ID + ".final_guard", ARMOUR_MOD_FINAL_GUARD = MOD_FINAL_GUARD + ARMOUR_SUFFIX;
 
     // industrial foregoing
 
@@ -104,7 +109,7 @@ public class NameConst {
     public static final String MAT_PINK_SLIME = "pink_slime";
     public static final String MAT_PINK_METAL = "pink_metal";
 
-    public static final String TRAIT_SLIMEY_PINK = "slimey_pink";
+    public static final String TRAIT_SLIMEY_PINK = TconEvoMod.MOD_ID + ".slimey_pink";
 
     public static final String ARMOUR_TRAIT_SLIMEY_PINK = TRAIT_SLIMEY_PINK + ARMOUR_SUFFIX;
 
