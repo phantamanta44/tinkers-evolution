b0bf12b4f18068fb1cd8633aa112e09159e388f7
phantamanta44 <evandalong@gmail.com>
2020-06-17 08:44:27 -0500

    Bump up to 1.0.14, only allow evasion to dodge attacks from entities and prevent spam-proccing it

--- src/main/java/xyz/phanta/tconevo/handler/EntityAttributeHandler.java
+++ src/main/java/xyz/phanta/tconevo/handler/EntityAttributeHandler.java
@@ -2,6 +2,7 @@ package xyz.phanta.tconevo.handler;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.SoundEvents;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundCategory;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
@@ -18,13 +19,19 @@ public class EntityAttributeHandler {
         // only do this on the server; since it's random, the server and client may produce different results
         // might cause some weird interactions where the client assumes the player got hit and does something funky
         // probably not a huge issue since mods shouldn't ever be handling game logic on the client anyways
-        if (victim.world.isRemote || event.getSource().isDamageAbsolute()
-                || DamageUtils.isPureDamage(event.getSource(), event.getAmount())) {
+        if (victim.world.isRemote) {
+            return;
+        }
+        DamageSource dmgSrc = event.getSource();
+        float amount = event.getAmount();
+        if (dmgSrc.getImmediateSource() == null || dmgSrc.isDamageAbsolute() || DamageUtils.isPureDamage(dmgSrc, amount)) {
             return;
         }
         double odds = victim.getEntityAttribute(TconEvoEntityAttrs.EVASION_CHANCE).getAttributeValue() - 1D;
         if (odds > 0D && (odds >= 1D || victim.world.rand.nextDouble() <= odds)) {
             event.setCanceled(true);
+            victim.lastDamage = amount;
+            victim.hurtResistantTime = victim.maxHurtResistantTime;
             victim.world.playSound(null, victim.posX, victim.posY, victim.posZ,
                     SoundEvents.ENTITY_PLAYER_ATTACK_SWEEP, SoundCategory.PLAYERS,
                     1F, 1.4F + 0.3F * victim.world.rand.nextFloat());
