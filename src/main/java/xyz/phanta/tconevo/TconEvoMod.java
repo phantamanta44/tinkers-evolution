74a9ffb725dd3562d43dfa8d84cf96c6fccf7c5e
phantamanta44 <evandalong@gmail.com>
2021-04-17 14:10:10 -0500

    Bump up to 1.0.40, remove transitive dependency on ccl from avaritia

--- src/main/java/xyz/phanta/tconevo/TconEvoMod.java
+++ src/main/java/xyz/phanta/tconevo/TconEvoMod.java
@@ -22,7 +22,7 @@ import xyz.phanta.tconevo.item.ItemMaterial;
 public class TconEvoMod extends Virtue {
 
     public static final String MOD_ID = "tconevo";
-    public static final String VERSION = "1.0.39";
+    public static final String VERSION = "1.0.40";
 
     @Mod.Instance(MOD_ID)
     public static TconEvoMod INSTANCE;
